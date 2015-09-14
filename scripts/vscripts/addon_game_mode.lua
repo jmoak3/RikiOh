57255183316177e21ceb35bb3405643a51cb2b7b
James Moak <mrjmoak3@gmail.com>
2015-09-14 07:21:23 -0400

    .

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -6,7 +6,7 @@ end
 
 function CRikiOhGameMode:OnHitByTower( event )
 	local player = PlayerResource:GetPlayer(event.PlayerID):GetAssignedHero()
-	player:SetHealth(player:GetHealth() + event.damage + 1)
+	player:SetHealth(player:GetHealth() + event.damage + 30)
 end
 
 function CRikiOhGameMode:OnEntityKilled( event )
@@ -17,10 +17,13 @@ function CRikiOhGameMode:OnEntityKilled( event )
 	
 	if not killedUnit:IsRealHero() or not hero:IsRealHero() then return nil end
 
-	if hero:GetClassname() == "npc_dota_hero_riki" and killedUnit:GetClassname() == "npc_dota_hero_sniper" then
+	if hero:GetClassname() == "npc_dota_hero_riki" and 
+			killedUnit:GetClassname() == "npc_dota_hero_sniper" then
 		local playerID = killedUnit:GetPlayerID()
-		PlayerResource:SetCustomTeamAssignment(playerID, heroTeam )
-		PlayerResource:ReplaceHeroWith(playerID, "npc_dota_hero_riki", 0, 0):ForceKill(false)
+		print(playerID)
+		PlayerResource:SetCustomTeamAssignment(playerID, DOTA_TEAM_BADGUYS)
+		local ent = PlayerResource:ReplaceHeroWith(playerID, "npc_dota_hero_riki", 0, 0)
+		ent:ForceKill(false)
 		print("Changing Team of killed player!")
 	end
 end
@@ -28,14 +31,15 @@ end
 function CRikiOhGameMode:OnGameInProgress()
 	print("on game start")
 	local numPlayers = 0
-	for team = 0, (DOTA_TEAM_COUNT-1) do
+	for team = 0,(DOTA_TEAM_COUNT-1) do
 	    numPlayers = numPlayers + PlayerResource:GetPlayerCountForTeam(team)
 		GameRules:SetCustomGameTeamMaxPlayers(team, 24)
 	end
-	for playerID = 1,numPlayers do
+	for playerID = 0,numPlayers-1 do
 	    PlayerResource:SetCustomTeamAssignment(playerID, DOTA_TEAM_GOODGUYS)
+		PlayerResource:ReplaceHeroWith(playerID, "npc_dota_hero_sniper", 0, 0)
 	end
-	local startingBadGuy = math.random(1, numPlayers) - 1
+	local startingBadGuy = math.random(0, numPlayers-1)
 	print(startingBadGuy)
 	PlayerResource:SetCustomTeamAssignment(startingBadGuy, DOTA_TEAM_BADGUYS)
 	local ent = PlayerResource:ReplaceHeroWith(startingBadGuy, "npc_dota_hero_riki", 0, 0)
@@ -60,6 +64,8 @@ function CRikiOhGameMode:InitGameMode()
 	CRikiOhGameMode.started = false
 	GameRules:GetGameModeEntity():SetFixedRespawnTime(10.0)
 	GameRules:GetGameModeEntity():SetCustomHeroMaxLevel(1)
+	GameRules:GetGameModeEntity():SetFogOfWarDisabled(true)
+	GameRules:SetCustomGameSetupRemainingTime(0.0)
 	for team = 0,(DOTA_TEAM_COUNT-1) do
 	    GameRules:SetCustomGameTeamMaxPlayers(team, 10)
 	end
@@ -69,6 +75,17 @@ function CRikiOhGameMode:OnThink()
 	if not CRikiOhGameMode.started and GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
 		self:OnGameInProgress()
 		CRikiOhGameMode.started = true
+		GameRules:SendCustomMessage("The Riki has been selected! Survive for 5 minutes, Snipers!", DOTA_TEAM_GOODGUYS, 1)
+	elseif GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
+		local survivors = PlayerResource:GetPlayerCountForTeam(DOTA_TEAM_GOODGUYS)
+		print("Survivors: "..survivors);
+		if survivors == 0 then
+			GameRules:MakeTeamLose(DOTA_TEAM_GOODGUYS)
+			GameRules:SendCustomMessage("The Rikis Win!", DOTA_TEAM_GOODGUYS, 1)
+		elseif GameRules:GetGameTime() > 390.0 then
+			GameRules:MakeTeamLose(DOTA_TEAM_BADGUYS)
+			GameRules:SendCustomMessage("The Snipers Win!", DOTA_TEAM_GOODGUYS, 1)
+		end
 	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
 		return nil
 	end
